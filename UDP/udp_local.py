3586860b9eb158db3a2c1bc11d9b7ade4ccb99e3
mvscti <marcus@mvscti.com.br>
2022-02-01 00:13:31 -0300

    Criando servidor echo

--- UDP/udp_local.py
+++ UDP/udp_local.py
@@ -9,28 +9,28 @@ def server(port):
     print('Listening at {}'.format(sock.getsockname()))
     while True:
         data, address = sock.recvfrom(MAX_BYTES)
-        text = data.decode('ascii')
-        print('The client at {} says {!r}'.format(address, text))
-        text = 'Your data was {} bytes long'.format(len(data))
-        data = text.encode('ascii')
+        text = data.decode('utf8')
+        print('O cliente em {} disse {!r}'.format(address, text))
+        text = 'Seu dado ocupa {} bytes'.format(len(data))
+        data = text.encode('utf8')
         sock.sendto(data, address)
 
 def client(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    text = 'The time is {}'.format(datetime.now())
-    data = text.encode('ascii')
+    text = 'A hora é {}'.format(datetime.now())
+    data = text.encode('utf8')
     sock.sendto(data, ('127.0.0.1', port))
-    print('The OS assigned me the address {}'.format(sock.getsockname()))
+    print('O  Sistema Operacional me atribui o endereço IP {}'.format(sock.getsockname()))
     data, address = sock.recvfrom(MAX_BYTES)  # Danger! See Chapter 2
-    text = data.decode('ascii')
-    print('The server {} replied {!r}'.format(address, text))
+    text = data.decode('utf8')
+    print('O servidor {} respondeu {!r}'.format(address, text))
 
 if __name__ == '__main__':
     choices = {'client': client, 'server': server}
-    parser = argparse.ArgumentParser(description='Send and receive UDP locally')
-    parser.add_argument('role', choices=choices, help='which role to play')
+    parser = argparse.ArgumentParser(description='Envia e recebe UDP localmente')
+    parser.add_argument('role', choices=choices, help='qual papel exercer')
     parser.add_argument('-p', metavar='PORT', type=int, default=1060,
-                        help='UDP port (default 1060)')
+                        help='UDP port (default 14000)')
     args = parser.parse_args()
     function = choices[args.role]
     function(args.p)
