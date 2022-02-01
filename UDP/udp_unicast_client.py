976cc1efecc2ccbbc388483a1b8d45a39332ea54
mvscti <marcus@mvscti.com.br>
2022-01-31 22:03:58 -0300

    Adicionando unicast e multicast

--- UDP/udp_unicast_client.py
+++ UDP/udp_unicast_client.py
@@ -1,14 +1,13 @@
 import argparse, socket, sys
 
 #endereço de boradcast
-BROADCAST='' #endereço de broadcast
-PORTA=10000 #porta ao qual o servidor estará "escutando"
+SERVER_ADDR='192.168.35.2' #informar endereço IP do servidor
+PORTA=25000 #porta ao qual o servidor estará "escutando"
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #cria um UDP/IP socket
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
 msg = input('Para sair use CTRL+X e pressione enter\n')
 while msg != '\x18':
 	#envia mensagem
-	s.sendto(msg.encode('utf8') , (BROADCAST, PORTA))
+	s.sendto(msg.encode('utf8') , (SERVER_ADDR, PORTA))
 	msg = input()
 print('Fechando socket....')
 s.close()
