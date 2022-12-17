6f7c11ea1188b875548359f2a9060af0a8c7fba2
Marcus Vinícius Costa <marcus@mvscti.com.br>
2022-12-17 19:08:28 -0300

    Corrigindo endereço multicast em servidor Windows

--- camada_transporte/udp_multicast_client.py
+++ camada_transporte/udp_multicast_client.py
@@ -1,15 +1,15 @@
 import socket
 
-MULTICAST_GROUP = '224.1.1.1'
+'''Para criar outros grupos, basta 'estartar' outros servidores com outros endereços IP multicast. 
+Para saber quais são os endereços IPv4 multicast reservados para redes locais, consulte https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml'''
+MULTICAST_GROUP = '224.1.1.1' 
 PORTA = 55555
 MULTICAST_TTL = 20 #Time to Live - número máximo de saltos até que o pacote possa ser descartado
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-s.setsockopt(socket.IPPROTO_IP, 
-                socket.IP_MULTICAST_TTL, 
-                MULTICAST_TTL)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 msg = input('Para sair use CTRL+X e pressione enter\n')
 while msg != '\x18':
 	#envia mensagem
-	s.sendto(msg.encode('utf8') ,(MULTICAST_GROUP, PORTA))
+	s.sendto(msg.encode('utf8') ,(MULTICAST_GROUP, PORTA)) #envia a mensagem para o endereço multicast
 	msg = input()
 print('Fechando socket....')
--- camada_transporte/udp_multicast_server.py
+++ camada_transporte/udp_multicast_server.py
@@ -1,19 +1,24 @@
-import socket, struct
+import socket, struct, sys
 
-MAX_BYTES = 65535
+MAX_BYTES = 65535 #Quantidade máxima (em Bytes) que uma mensagem pode ter
 PORTA  = 55555
-MULTICAST_GROUP = '224.1.1.1'
+
+'''Para criar outros grupos, basta 'estartar' outros servidores com outros endereços IP multicast. 
+Para saber quais são os endereços IPv4 multicast reservados para redes locais, consulte https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml'''
+MULTICAST_GROUP = '224.1.1.1' 
+
 HOST_ADDR    =  socket.gethostbyname(socket.gethostname()) #recebe o endereço IP do adaptador
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((MULTICAST_GROUP, PORTA))
-pacote=struct.pack('4sl', 
-                    socket.inet_aton(MULTICAST_GROUP), 
-                    socket.INADDR_ANY)
-s.setsockopt(socket.IPPROTO_IP, 
-            socket.IP_ADD_MEMBERSHIP, 
-            pacote)
-print('Servidor multicast escutando na porta', format(PORTA))
+#Verifica se o Sistema Operacional é Windows ou Linux
+if sys.platform == 'win32':
+	s.bind(('', PORTA))
+else:
+      s.bind((MULTICAST_GROUP, PORTA))
+pacote=struct.pack('4sl', socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, pacote)
+print(f'Servidor multicast escutando na porta {PORTA}. Para encerrar o servidor, é necessário fechar o terminal.')
+#Executa um loop 'infinito'
 while True:
-    message, address = s.recvfrom(MAX_BYTES)
-    print(address , ' disse ',message.decode('utf8'))
+	mensagem, endereco = s.recvfrom(MAX_BYTES) #aguarda receber mensagens 
+	print(f"{address} disse {message.decode('utf8')}") #.decode('utf8'): converte a mensagem recebida no padrão utf-8
